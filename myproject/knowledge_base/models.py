3a1b43fad981baa9c701981b5cc87396fd9fbe24
Vladislav Kuznetsov <headgenrekty+github@gmail.com>
2025-11-14 13:02:26 +0400

    Добавлена модель Directory

--- myproject/knowledge_base/models.py
+++ myproject/knowledge_base/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Directory(models.Model):
+    """Иерархическая модель папок/категорий базы знаний"""
+    name = models.CharField(max_length=255, verbose_name='Название папки')
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='subdirectories',
+        verbose_name='Родительская категория'
+    )
+    order = models.PositiveIntegerField(default=0, verbose_name='Порядок сортировки')
+
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['order', 'name']
+        indexes = [
+            models.Index(fields=['parent'], name='directory_parent_idx'),
+        ]
+
+    def __str__(self):
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
--- myproject/knowledge_base/views.py
+++ myproject/knowledge_base/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Directory
 
 
 
 class KbHome(TemplateView):
     template_name = 'knowledge_base/kb_home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        
+        # Корневые папки
+        root_dirs = Directory.objects.filter(parent__isnull=True)
+
+        context['directories'] = [dir.id for dir in root_dirs]
+
+        return context
