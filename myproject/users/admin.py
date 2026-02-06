c8b6144a2ccd61bd617e508d1d385d73884827c3
Vladislav Kuznetsov <headgenrekty+github@gmail.com>
2026-02-06 19:03:01 +0400

    upd readme

--- myproject/users/admin.py
+++ myproject/users/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'bio']
-    fields = ['user', 'image', 'bio']
-    readonly_fields = ['user', 'image', 'bio']
-    search_fields = ['user__username', 'bio']
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ['user', 'bio']
+#     fields = ['user', 'image', 'bio']
+#     readonly_fields = ['user', 'image', 'bio']
+#     search_fields = ['user__username', 'bio']
 
