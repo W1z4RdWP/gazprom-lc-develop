10d60de5cb8fdce649b444a650b7b8f0a61af8d6
w1z4rdWP <headgenrekty@gmail.com>
2026-02-27 20:02:23 +0400

    Удалены лишние шаблоны

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -413,24 +413,6 @@ class CustomLoginView(LoginView):
         return redirect(self.get_success_url())
 
 
-@login_required
-def get_user_progress(request):
-    quizresults = QuizResult.objects.all()
-    user_courses = UserCourse.objects.all()
-
-    quizzes_attempt = quizresults.filter(user=request.user).count()
-    total_courses = user_courses.filter(user=request.user).count()
-    completed_courses = user_courses.filter(user=request.user, is_completed=True).count() 
-
-    context = {
-        'total_courses': total_courses,
-        'completed_courses': completed_courses,
-        'quizzes_attempts': quizzes_attempt
-    }
-
-    return render(request, 'users/user_progress.html', context=context)
-
-
 
 
 class CreateGroupView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
